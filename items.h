ba526efdab60809e07bb4ba3903f9b69e5e614ee
Finian Wright <turbowafflz@gmail.com>
2020-04-25 14:07:58 -0400

    Lowercase potion name

--- items.h
+++ items.h
@@ -3,7 +3,7 @@ sword.name = "sword";
 sword.description = "It's a sword";
 sword.attackDamage = 5;
 item orcEye;
-orcEye.name = "Potion";
+orcEye.name = "potion";
 orcEye.description = "Recovers 5 HP";
 orcEye.healthEffect = 5;
 orcEye.consumable = true;
